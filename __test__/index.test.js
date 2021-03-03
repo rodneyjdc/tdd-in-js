f48cde3dc1dadf303758eb0609af0a5fd3b8fa6a
Rodney Canlas <canlasrj@gmail.com>
2021-03-03 11:27:14 -0800

    Completed divide function.

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,5 +1,126 @@
 const base = require('../index');
 
+// add
 test('add 1 + 2 to equal 3', () => {
     expect(base.add([1,2])).toBe(3);
 })
+
+test('add 0 + 2 to equal 2', () => {
+    expect(base.add([0, 2])).toBe(2);
+})
+
+test('add 0 + 2 + 3 to equal 5', () => {
+    expect(base.add([0, 2, 3])).toBe(5);
+})
+
+test('add 0 to equal 0', () => {
+    expect(base.add([0])).toBe(0);
+})
+
+test('pass empty array', () => {
+    expect(base.add([])).toBe(undefined);
+})
+
+test('add a string to return a string', () => {
+    expect(base.add(['0'])).toBe('0');
+})
+
+test('add two strings to return a string', () => {
+    expect(base.add(['0', '1'])).toBe('01');
+})
+
+test('add a string and a number to return a string', () => {
+    expect(base.add(['1', 100])).toBe('1100');
+})
+
+// subtract
+
+test('subtract 1 - 2 to equal -1', () => {
+    expect(base.subtract([1, 2])).toBe(-1);
+})
+
+test('subtract 0 - 2 to equal -2', () => {
+    expect(base.subtract([0, 2])).toBe(-2);
+})
+
+test('subtract 0 - 2 - 3 to equal -5', () => {
+    expect(base.subtract([0, 2, 3])).toBe(-5);
+})
+
+test('subtract 0 to equal 0', () => {
+    expect(base.subtract([0])).toBe(0);
+})
+
+test('pass empty array', () => {
+    expect(base.subtract([])).toBe(undefined);
+})
+
+test('subtract a string to return a string', () => {
+    expect(base.subtract(['0'])).toBe('0');
+})
+
+test('subtract \'0\' - \'1\' to equal a -1', () => {
+    expect(base.subtract(['0', '1'])).toBe(-1);
+})
+
+test('subtract \'bill\' - \'parrot\' to return NaN', () => {
+    expect(base.subtract(['bill', 'parrot'])).toBe(NaN);
+})
+
+test('subtract \'1\' - 100 to equal -99', () => {
+    expect(base.subtract(['1', 100])).toBe(-99);
+})
+
+// divide
+
+test('divide 1 / 1 to equal 1', () => {
+    expect(base.divide([1, 1])).toBe(1);
+})
+
+test('divide 1 / 0 to equal Cannot divide by zero', () => {
+    //expect(base.divide([1, 0])).toBe("Error");
+    expect(base.divide([1, 0])).toBe("Cannot divide by zero");
+})
+
+test('divide 1 / "0" to equal Cannot divide by zero', () => {
+    //expect(base.divide([1, 0])).toBe("Error");
+    expect(base.divide([1, "0"])).toBe("Cannot divide by zero");
+})
+
+test('divide 1 / 0 / 1 to equal Cannot divide by zero', () => {
+    //expect(base.divide([1, 0, 1])).toBe("Error");
+    expect(base.divide([1, 0, 1])).toBe("Cannot divide by zero");
+})
+
+test('divide 0 / 1 to equal 0', () => {
+    expect(base.divide([0, 1])).toBe(0);
+})
+
+test('divide "0" / 1 to equal 0', () => {
+    expect(base.divide(["0", 1])).toBe(0);
+})
+
+test('divide 1 / "0" / 1 to equal Cannot divide by zero', () => {
+    //expect(base.divide([1, "0", 1])).toBe("Error");
+    expect(base.divide([1, 0, 1])).toBe("Cannot divide by zero");
+})
+
+test('divide 1 / "bill" / 1 to equal Cannot divide these', () => {
+    expect(base.divide([1, "bill", 1])).toBe("Cannot divide these");
+})
+
+test('divide "bill" / 1 to equal Cannot divide these', () => {
+    expect(base.divide(["bill", 1])).toBe("Cannot divide these");
+})
+
+test('pass in empty array to divide to equal undefined', () => {
+    expect(base.divide([])).toBe(undefined);
+})
+
+// multiple
+
+test('multiple 1 * 1 to equal 1', () => {
+    expect(base.multiple([1, 1])).toBe(1);
+})
+
+
--- index.js
+++ index.js
@@ -1,17 +1,53 @@
+
+
 function add (input) {
        return operation(input, function (result, input) {
             return result + input;
         })
     }
 
-function operation(input, operator) {
-    var result = input.shift();
-    for (i = 0; i < input.length; i++) {
-        result = operator(result, input[i]);
+function subtract (input) {
+    try {
+        return operation(input, function (result, input) {
+            return result - input;
+        });
+    } catch (error) {
+        return "Cannot subtract two strings"
     }
+}
 
-    return result;
+function divide (input) {
+    return operation(input, function (result, input) {
+        if (input == 0 || result === "Cannot divide by zero") {
+            return "Cannot divide by zero";
+        } else if (!Number.isFinite(result / input)) {
+            return "Cannot divide these";
+        } else if (result === "Error") {
+            return "Error";
+        }
+
+        return result / input;
+    });
+}
+
+function multiple (input) {
+    return input;
 }
 
 
-module.exports = { add, operation}
+
+function operation(input, operator) {
+    try {
+        var result = input.shift();
+        for (i = 0; i < input.length; i++) {
+            result = operator(result, input[i]);
+        }
+        return result;
+    } catch (error) {
+        return "Cannot perform operation"
+    }
+    
+}
+
+
+module.exports = { add, subtract, divide, multiple, operation}
