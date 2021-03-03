1c7f4e6ab56943b1a1e75525cf76f91c0a2e1f71
rvehall <rvehall@gmail.com>
2021-03-03 08:50:33 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function Calculator() {
+    this.add = function (input) {
+        return operation(input, function (result, input) {
+            return result + input;
+       
+        })
+    }
+    function operation(input, operator) {
+        var result = input.shift();
+        for (i = 0; i < input.length; i++) {
+            result = operator(result, input[i]);
+        }
+    
+        return result;
+    }
+}
+
+
+module.exports = { Calculator }
